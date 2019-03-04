818d373b0bfa31045547c34f26ea079d28570f57
colonP173 <vanurag.173@gmail.com>
2019-03-05 00:02:59 +0530

    Add files via upload

---
+++ dv.py
@@ -0,0 +1,55 @@
+
+
+# Step 1: For each node prepare the destination and predecessor
+def initialize(graph, source):
+    d = {} # Stands for destination
+    p = {} # Stands for predecessor
+    for node in graph:
+        d[node] = float('Inf') # We start admiting that the rest of nodes are very very far
+        p[node] = None
+    d[source] = 0 # For the source we know how to reach
+    return d, p
+
+def relax(node, neighbour, graph, d, p):
+    # If the distance between the node and the neighbour is lower than the one I have now
+    if d[neighbour] > d[node] + graph[node][neighbour]:
+        # Record this lower distance
+        d[neighbour]  = d[node] + graph[node][neighbour]
+        p[neighbour] = node
+
+def bellman_ford(graph, source):
+    d, p = initialize(graph, source)
+    for i in range(len(graph)-1): #Run this until is converges
+        for u in graph:
+            for v in graph[u]: #For each neighbour of u
+                relax(u, v, graph, d, p) #Lets relax it
+
+    # Step 3: check for negative-weight cycles
+    for u in graph:
+        for v in graph[u]:
+            assert d[v] <= d[u] + graph[u][v]
+
+    return d, p
+
+
+def main():
+    """graph = {
+        'a': {'b': -1, 'c':  4},
+        'b': {'c':  3, 'd':  2, 'e':  2},
+        'c': {},
+        'd': {'b':  1, 'c':  5},
+        'e': {'d': -3}
+        }"""
+    graph = {
+        'a': {'b': 2, 'c':  50},
+        'b': {'a' : 2, 'c' : 3},
+        'c': {'a' : 50, 'b' : 3}
+        }
+
+
+    d, p = bellman_ford(graph, 'a')
+    print(d,p)
+
+
+
+if __name__ == '__main__': main()
